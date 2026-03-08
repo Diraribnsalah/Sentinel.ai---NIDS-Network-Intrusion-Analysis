957358ad663f174dce92ccf841d07f8307ca9ec5
OMAR JEBBARI <143851249+OmarJebbari@users.noreply.github.com>
2026-03-08 20:31:39 +0000

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/Sentinel.ai---NIDS-Network-Intrusion-Analysis/',
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Sentinel.ai---NIDS-Network-Intrusion-Analysis/',
       server: {
         port: 3000,
         host: '0.0.0.0',
